4ad994fe40778df081269efdf6a08195b8d04801
Ash Trull <ashtrull@gmail.com>
2017-09-06 22:20:26 -0400

    Test add js

--- js/init.js
+++ js/init.js
@@ -6,3 +6,8 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$('#bio-preview').on('click', function () {
+  $('.middle').hide()
+  $('#full-bio').append('.row.center')
+})
