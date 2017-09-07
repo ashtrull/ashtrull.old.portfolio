e02ca658c13faf276d6351de26967068bd1bc3e2
Ash Trull <ashtrull@gmail.com>
2017-09-06 22:29:56 -0400

    Add js About Me and My Work

--- js/init.js
+++ js/init.js
@@ -4,10 +4,14 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+    $('#bio-button').on('click', function () {
+      $('.middle').hide()
+      $('#full-bio').append('.row.center')
+    })
+    $('#work-button').on('click', function () {
+      $('.middle').hide()
+      $('#kit-cat-go').show()
+    })
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-
-$('#bio-preview').on('click', function () {
-  $('.middle').hide()
-  $('#full-bio').append('.row.center')
-})
