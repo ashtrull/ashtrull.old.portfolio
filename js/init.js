d1cc4104fd10934de325ec26e615dd7dd6df03ce
Ash Trull <ashtrull@gmail.com>
2017-09-06 22:59:27 -0400

    Adjust image size, hide buttons

--- js/init.js
+++ js/init.js
@@ -5,14 +5,20 @@
     $('.parallax').parallax()
     $('#full-bio').hide()
     $('#kit-cat-go').hide()
+    $('#show-less-button').hide()
+    $('#hide-button').hide()
 
     $('#bio-button').on('click', function () {
       $('.middle').hide()
       $('#full-bio').show()
+      $('#bio-button').hide()
+      $('#show-less-button').show()
+
     })
     $('#work-button').on('click', function () {
       $('.middle').hide()
       $('#kit-cat-go').show()
+      $('#hide-button').show()
     })
 
   }); // end of document ready
